9107b8cfbf2d0f0c16f0d88758a802cef3a80420
francescoliguori <41292339+francescoliguori@users.noreply.github.com>
2018-11-15 09:14:40 +0100

    replace with for instruction
    replace with for instruction

--- src/gioco/prova/gfx/Assets.java
+++ src/gioco/prova/gfx/Assets.java
@@ -32,29 +32,36 @@ public class Assets {
         playerStop = new BufferedImage[3];
         playerDown = new BufferedImage[4]; //my add
         player = sheet.crop(0, 0 , width, height);
-        playerRunning[0] = sheet.crop(0, height, width, height);
+        for(int i=0;i<=5;i++){
+            playerRunning[i] = sheet.crop(width*i, height, width, height);
+        }
+        /*playerRunning[0] = sheet.crop(0, height, width, height);
         playerRunning[1] = sheet.crop(width * 1, height, width, height);
 	playerRunning[2] = sheet.crop(width * 2, height, width, height);
 	playerRunning[3] = sheet.crop(width * 3, height, width, height);
 	playerRunning[4] = sheet.crop(width * 4, height, width, height);
-        playerRunning[5] = sheet.crop(width * 5, height, width, height);
-        
-        playerJump[0] = sheet.crop(0, height, width, height);
+        playerRunning[5] = sheet.crop(width * 5, height, width, height);*/
+        for(int i=0;i<=7;i++){
+            playerJump[i] = sheet.crop(width*i, height*3, width, height);
+        }
+        /*playerJump[0] = sheet.crop(0, height, width, height);
         playerJump[1] = sheet.crop(width * 1, height*3, width, height);
 	playerJump[2] = sheet.crop(width * 2, height*3, width, height);
 	playerJump[3] = sheet.crop(width * 3, height*3, width, height);
 	playerJump[4] = sheet.crop(width * 4, height*3, width, height);
         playerJump[5] = sheet.crop(width * 5, height*3, width, height);
         playerJump[6] = sheet.crop(width * 6, height*3, width, height);
-        playerJump[7] = sheet.crop(width * 7, height*3, width, height);
+        playerJump[7] = sheet.crop(width * 7, height*3, width, height);*/
         playerStop[0] = sheet.crop(0, height * 2, width, height);
-        
-        //playerDown[0] = sheet.crop(0, height*4, width, height);  //my add 
+        for(int i=1;i<=3;i++){
+            playerDown[i] = sheet.crop(width*i, height*4, width, height);
+        }
+        /*playerDown[0] = sheet.crop(0, height*4, width, height);  //my add 
         playerDown[0] = sheet.crop(width * 1, height*4, width, height);
 	playerDown[1] = sheet.crop(width * 2, height*4, width, height);
         playerDown[2] = sheet.crop(width * 3, height*4, width, height);
 	playerDown[3] = sheet.crop(width * 4, height*4, width, height);
-        //playerDown[5] = sheet.crop(width * 5, height*4, width, height);
+        playerDown[5] = sheet.crop(width * 5, height*4, width, height);*/
 	
     }
 }
