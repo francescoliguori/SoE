9d8c27516642445ec5757dc30cfc513c7b23a93e
giodice <38973295+giodice@users.noreply.github.com>
2018-12-14 13:36:08 +0100

    Update Fireball
    - fireball cancella anche i bullets dei nemici

--- src/gioco/prova/bullets/Fireball.java
+++ src/gioco/prova/bullets/Fireball.java
@@ -13,49 +13,67 @@ import gioco.prova.gfx.Assets;
 import java.awt.Color;
 import java.awt.image.BufferedImage;
 
-
 /**
  *
  * @author marcoruggiero
  */
 public class Fireball extends Entity {
-    
+
     private Animation fireballJutsu;
-    
-    public Fireball(Handler handler, float x, float y, int width, int height){
-       super(handler,  x,  y,  width,  height);
-       fireballJutsu = new Animation(240, Assets.fireballJutsu);
-       bounds.x = 100;
-       bounds.y = 60;
-       bounds.width = 123;
-       bounds.height = 120;
-      
+
+    public Fireball(Handler handler, float x, float y, int width, int height) {
+        super(handler, x, y, width, height);
+        fireballJutsu = new Animation(240, Assets.fireballJutsu);
+        bounds.x = 100;
+        bounds.y = 60;
+        bounds.width = 123;
+        bounds.height = 120;
+
     }
 
     @Override
-    public void tick(){
-      
-       move();
-       
+    public void tick() {
+        move();
         fireballJutsu.tick();
+        checkArrowCollisions(0, 0);
+        checkKunaiCollisions(0, 0);
     }
 
     //@Override
     public void render(Graphics g) {
- 
-         g.drawImage(getCurrentAnimationFrame(), (int) x+40, (int) y-15, null);
-         //g.setColor(Color.red);
-         //g.fillRect((int)x+bounds.x , (int)y+bounds.y , bounds.width, bounds.height);
-       
+
+        g.drawImage(getCurrentAnimationFrame(), (int) x + 40, (int) y - 15, null);
+//        g.setColor(Color.red);
+//        g.fillRect((int) x + bounds.x, (int) y + bounds.y, bounds.width, bounds.height);
+
     }
-    
-    private BufferedImage getCurrentAnimationFrame(){
-        return fireballJutsu.getCurrentFrame();   
+
+    private BufferedImage getCurrentAnimationFrame() {
+        return fireballJutsu.getCurrentFrame();
     }
-    
-    public void move(){
-        x+=10;
+
+    public void move() {
+        x += 10;
     }
-     
-    
+
+    public boolean checkKunaiCollisions(float xOffset, float yOffset) {
+        for (Kunai k : handler.getGame().getGameState().getController().getListKunaiEnemies()) {
+            if (k.getCollisionBounds(0f, 0f).intersects(this.getCollisionBounds(xOffset, yOffset))) {
+                handler.getGame().getGameState().getController().removeKunaiEnemies(k);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean checkArrowCollisions(float xOffset, float yOffset) {
+        for (Arrow a : handler.getGame().getGameState().getController().getListArrowEnemies()) {
+            if (a.getCollisionBounds(0f, 0f).intersects(this.getCollisionBounds(xOffset, yOffset))) {
+                handler.getGame().getGameState().getController().removeArrowEnemies(a);
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
